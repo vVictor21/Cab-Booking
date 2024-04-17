6ca0cb02b4a638d14c8b1d8c96d2cf39f24ca759
vVictor21 <vv_dany@yahoo.co.uk>
2024-04-17 12:52:15 +0300

    created database

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ import time
 from tkinter import messagebox as ms
 import sqlite3
 
-class user:
-    def __init__(self,master):
+with sqlite3.connect('Users.db') as db:
+    c = db.cursor()
+
+c.execute('CREATE TABLE IF NOT EXISTS User(username TEXT NOT NULL, password TEXT NOT NULL)')
+db.commit()
+db.close()
+
+class User:
+    def __init__(self, master):
         self.master = master
         self.username = StringVar()
         self.password = StringVar()
         self.n_username = StringVar()
         self.n_password = StringVar()
         self.widgets()
+
 
 if __name__ == '__main__':
     root = Tk()
@@ -21,5 +29,5 @@ if __name__ == '__main__':
     geometry = '%dx%d+%d+%d' % (w, h, 50, 0)
     root.geometry("500x300+320+200")
     root.title("Login Form")
-    application = user(root)
+    application = User(root)
     root.mainloop()
